cbb7f2453f4214a0da1653b7798819b36ef27845
Vishal Shah <60571008+suhvishalp@users.noreply.github.com>
2026-02-18 13:24:27 +0530

    Feature/desks admin vishal 01 (#1)
    * Add Desk class
    
    * Add DeskDAO interface to describe methods to perform db operations
    
    * Add JDBCDeskDao class with logic to perfom db operations
    
    * Add Desk Model class
    
    * Add DeskService class with business logic
    
    * Add Deskcontroller to display desk menu
    
    * Update AppConfig with menus for deskcontroller
    
    * Update Main menu with desk operations
    
    ---------
    
    Co-authored-by: vishalshah <vishalshah@localhost>

--- src/main/java/com/deskmate/config/AppConfig.java
+++ src/main/java/com/deskmate/config/AppConfig.java
@@ -1,5 +1,16 @@
 package com.deskmate.config;
 
+import com.deskmate.controller.DeskController;
+import com.deskmate.dao.DeskDao;
+import com.deskmate.dao.impl.JdbcDeskDao;
+import com.deskmate.services.DeskService;
+
 public class AppConfig {
+
+	public DeskController deskController() {
+        DeskDao deskDao = new JdbcDeskDao();
+        DeskService deskService = new DeskService(deskDao);
+        return new DeskController(deskService);
+    }
 
 }
